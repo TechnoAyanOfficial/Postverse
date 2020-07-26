117e640cf815b9f8c8a6db4eb00df6c948ccfa45
Ayan Ansari <62174456+TechnoAyanOfficial@users.noreply.github.com>
2020-07-26 11:12:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,12 +2,13 @@ from pyrogram import Client, Filters, InlineKeyboardButton, InlineKeyboardMarkup
 import tgcrypto
 import pyrogram.errors
 app = Client("Postverse", api_id=938340, api_hash="4c47ceee0c51daa3e6608dd728c6148d", bot_token="1324991431:AAEU-NKyoEbedSYv94JkdSrWBkcAM_m0mNA")
-groups = ['gforgroup', 'pyrogramchat', 'ggggggggaka']
-admins = [1074490547, 1111847352]
+groups = ['gforgroup', 'ggggggggaka']
+admins = [1074490547, 1111847352, 820463901]
 @app.on_message(Filters.command(['start']))
 
 def download(client, message):
-	message.reply_text('hello!')
+	message.reply_text('Just Send Me Any Text Message I'll Forward It To All Saved Groups!
+')
 	
 @app.on_message(Filters.text)
 def download(client, message):
